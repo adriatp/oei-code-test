39bdb97d702a9cff78d58f55fcf140a127c9993b
adriatp <tarradas.adria@gmail.com>
2023-08-01 23:27:05 +0200

    Apply rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+  namespace :api_app do
+    namespace 'v1' do
+      post 'editions/filter', to: 'editions#filter'
+    end
+  end
 end
